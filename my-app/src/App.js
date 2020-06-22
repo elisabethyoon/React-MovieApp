740dc7be99005101f7b5aac68dc95dce0903701b
betty <betty@marketdesigners.com>
2020-06-22 23:34:49 +0900

    isRequired, setState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { Component }from 'react';
 import './App.css';
 import Movie from './Movie'
 
-const movieTitles = [
-  "Martrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
+const movies =[
+  {
+    title: "Matrix",
+    poster:"https://upload.wikimedia.org/wikipedia/ko/thumb/2/26/%EB%A7%A4%ED%8A%B8%EB%A6%AD%EC%8A%A4_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg/220px-%EB%A7%A4%ED%8A%B8%EB%A6%AD%EC%8A%A4_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg",
+  },
+  {
+    title: "Full Metal Jacket",
+    poster:"https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
+  },
+  {
+    title: "Oldboy",
+    poster:"https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
+  },
+  {
+    title: "Star Wars",
+    poster:"https://secimage.yes24.com/goods/22343246/L"
+  },
 ]
 
-const movieImages = [
-  "https://lh3.googleusercontent.com/proxy/zasogXLCIw1trzD5RzAJYH72jdyo6o00RMRFAnqSGGim_-y-cQYms2VnTnXyhRQuALMMuNWHm095aPu6ELmXLO_ueV9o2kOufNuzoAPZJ5K8VpeFRtQ5",
-  "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/6/67/Oldboykoreanposter.jpg",
-  "https://pbs.twimg.com/media/ELbvxLKVAAEU9dn.jpg"
-]
+class App extends Component {
+  
+  state = {
+    greeting: 'hello!'
+  }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        greeting: 'hello again!'
+      })
+    }, 2000)
+  }
 
-
-function App() {
-  return (
-    <div className="App">
-      <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-      <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-      <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-      <Movie title={movieTitles[3]} poster={movieImages[3]}/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        {this.state.greeting}
+        {movies.map( (movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
+        })}
+      </div>
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {movies.map(movie => {
+//         return <Movie title={movie.title} poster={movie.poster}/>
+//       })}
+//     </div>
+//   );
+// }
 
 export default App;
