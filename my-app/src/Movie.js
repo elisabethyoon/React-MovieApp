4040d7bd0452fec92719b1baee5b8ca4a3e64fba
betty <betty@marketdesigners.com>
2020-06-25 00:02:11 +0900

    smart vs Dumb component
    class형 component -> 함수형 component 변환

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -1,37 +1,71 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
+// class 형 컴포넌트
+// class Movie extends Component{
 
-	static propTypes = {
-		title: PropTypes.string.isRequired,
-		poster: PropTypes.string.isRequired
-	}
+// 	static propTypes = {
+// 		title: PropTypes.string.isRequired,
+// 		poster: PropTypes.string.isRequired
+// 	}
 
 
-	render(){
-		return(
-			<div>
-			<h1>{this.props.title}</h1>
-			<MoviePoster poster={this.props.poster}/>
-			</div>
-		)
-	 }
+// 	render(){
+// 		return(
+// 			<div>
+// 				<h1>{this.props.title}</h1>
+// 				<MoviePoster poster={this.props.poster}/>
+// 			</div>
+// 		)
+// 	 }
+// }
+
+// 함수형 컴포넌트
+function Movie({title, poster}){
+	return (
+		<div>
+			<h1>{title}</h1>
+			<MoviePoster poster={poster}/>
+		</div>
+	)
 }
 
-class MoviePoster extends Component{
+// 함수형 propsTypes check
+Movie.propTypes = {
+	title: PropTypes.string.isRequired,
+	poster: PropTypes.string.isRequired
+}
 
-	static propTypes = {
-		 poster: PropTypes.string.isRequired
-	}
 
-	render(){
-		console.log(this.props)
-		return(
-			<img src={this.props.poster}/>
-		)
-	}
+
+// class 형 컴포넌트
+// class MoviePoster extends Component{
+
+// 	static propTypes = {
+// 		 poster: PropTypes.string.isRequired
+// 	}
+
+// 	render(){
+// 		console.log(this.props)
+// 		return(
+// 			<img src={this.props.poster} alt="Movie Poster"/>
+// 		)
+// 	}
+// }
+
+
+// 함수형 컴포넌트
+function MoviePoster({poster}) {
+	return (
+		<img src={poster} alt="Movie Poster"/>
+	)
+}
+
+// 함수형 propsTypes check
+MoviePoster.propTypes = {
+	poster: PropTypes.string.isRequired
 }
 
 export default Movie
