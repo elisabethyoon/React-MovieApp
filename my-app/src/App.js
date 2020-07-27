f8ded7adc241074b2f557d93690aafbb176bebe7
betty <betty@marketdesigners.com>
2020-07-27 18:08:13 +0900

    event state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -45,7 +45,9 @@ class App extends Component {
   constructor(props){ // 컴포넌트가 실행될 때 constructor라는 함수가 있으면 젤 먼저 실행되서 초기화를 담당한다.
     super(props);
     this.state = {
+      mode: 'read',
       subject: {title:'web', sub: 'world wide web'},
+      welcome:{title:'welcome', desc:'hello, react!!'},
       contents: [
         {id:1, title:'HTML', desc: 'HTML is aaaa'},
         {id:2, title:'CSS', desc: 'CSS is bbbb'},
@@ -55,6 +57,16 @@ class App extends Component {
   }
 
   render() {
+    // 생활코딩 리액트
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       // 노마드코더 영화앱
       // <div className="App">
@@ -66,12 +78,23 @@ class App extends Component {
 
       // 생활코딩 리액트
       <div className="App">
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            // this.state.mode = 'welcome';
+            this.setState({
+              mode:'welcome'
+            })
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is dkdkdk"></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     )
   }
